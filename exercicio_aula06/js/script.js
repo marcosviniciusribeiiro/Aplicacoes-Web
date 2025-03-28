4ae9a29712b15e936aad3611f349c6b0146e7fcf
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-03-28 12:33:57 -0300

    exercicio 06 funcionando

--- exercicio_aula06/js/script.js
+++ exercicio_aula06/js/script.js
@@ -1,4 +1,26 @@
-function Calcular() {
-  let num = document.getElementById("num").value;
-  alert(num);
+var inputHTML = document.getElementById("fnum");
+var res = document.getElementById("res");
+
+var botão = document.getElementById("btn");
+botão.addEventListener("click", CalcularFibonacci);
+
+function Fibonacci(n, cache = {}) { //guarda o valor de um Fibonacci assim que ele é calculado
+  if (n in cache) return cache[n];
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  cache[n] = Fibonacci(n - 1, cache) + Fibonacci(n - 2, cache);
+  return cache[n];
+}
+
+function CalcularFibonacci() {
+  let num = Number(inputHTML.value);
+  if (isNaN(num) || num < 0 || !Number.isInteger(num)) {
+    alert("Digite um Valor Positivo!");
+    return;
+  }
+  let resultado = Fibonacci(num);
+  res.innerHTML = `Fibonacci(${num}) = ${resultado}`;
+
+  inputHTML.value = "";
+  inputHTML.focus();
 }
