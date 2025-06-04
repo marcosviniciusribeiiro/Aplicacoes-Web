b6afe60a8925eed9c346eb1a69fdb77c4c8f77cf
MarcosVinicius <Marcosvini.rg@gmail.com>
2025-06-04 07:45:28 -0300

    add js

--- aula14/js/adicionar.js
+++ aula14/js/adicionar.js
@@ -1,15 +1,18 @@
-function adicionar(nome, data, hora) {
-  if (!nome || !data || !hora) return;
-
+function adicionar(nome, usuario, data, hora) {
+  if (!nome || !data || !hora || !usuario) return;
   let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
   let tarefa = {
     nome: nome,
+    usuario: usuario,
     data: data,
-    hora: hora
+    hora: hora,
   };
   tarefas.push(tarefa);
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
   document.getElementById("tarefa").value = "";
+  document.getElementById("usuario").value = "";
+  document.getElementById("data").value = "";
+  document.getElementById("hora").value = "";
 }
 
 function irParaVisualizar() {
--- aula14/js/visualizar.js
+++ aula14/js/visualizar.js
@@ -5,16 +5,41 @@ function carregarTarefa() {
 
   tarefas.forEach(function (tarefa) {
     const li = document.createElement("li");
-    li.textContent = `Tarefa: ${tarefa.nome} / Data: ${tarefa.data} ás ${tarefa.hora} horas`;
+    li.innerHTML = `
+       <label class="list-group-item d-flex gap-3">
+      <input
+        class="form-check-input flex-shrink-0"
+        type="checkbox"
+        value=""
+        checked
+        style="font-size: 1.375em"
+      />
+      <span class="pt-1 form-checked-content">
+        <h5>Tarefa: ${tarefa.nome}</h5>
+        <strong>tarefa do(a): ${tarefa.usuario}</strong>
+        <small class="d-block text-body-secondary">
+          <svg
+            class="bi me-1"
+            width="1em"
+            height="1em"
+            role="img"
+            aria-label="Schedule"
+          >
+            <use xlink:href="#calendar-event"></use>
+          </svg>
+          data: ${tarefa.data} / hora: ${tarefa.hora}
+        </small>
+      </span>
+    </label>`;
     lista.appendChild(li);
   });
 }
 
 function criarTarefas() {
   let tarefasIniciais = [
-    { nome: "Estudar JavaScript", data: "26-05-2025", hora: "15:30" },
-    { nome: "Fazer exercício", data: "26-05-2025", hora: "17:00" },
-    { nome: "Ler um livro", data: "26-05-2025", hora: "20:00" },
+    { nome: "Estudar JavaScript", usuario: "Marcos", data: "26-05-2025", hora: "15:30"},
+    { nome: "Fazer exercício", usuario: "João", data: "26-05-2025", hora: "17:00"},
+    { nome: "Ler um livro", usuario: "Felipe", data: "26-05-2025", hora: "20:00" },
   ];
 
   localStorage.setItem("tarefas", JSON.stringify(tarefasIniciais));
